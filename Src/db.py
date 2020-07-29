e8ca3021189d04f110fb6cddb5e1f660ea9ef5be
zimerman <zr@sem-wolf.org>
2020-07-29 18:32:57 +0300

    insert,delete and update record by index

--- Src/db.py
+++ Src/db.py
@@ -48,31 +48,31 @@ class DBTable(db_api.DBTable):
         if None == values.get(self.key_field_name):
             raise ValueError
         data_table = shelve.open(f"db_files/{self.name}.db",writeback=True)
-        try:
-            if data_table.get(str(values[self.key_field_name])):
-                data_table.close()
-                raise ValueError
-            if len(self.fields)<len(values.keys()):
-                data_table.close()
-                raise ValueError
-            data_table[str(values[self.key_field_name])] = {}
-            for field in self.fields:
-                if self.is_index(field.name):
-                    if values.get(field.name) == None:
-                        raise ValueError
-            for field in self.fields:
-                if self.is_index(field.name):
-                    db_index = shelve.open(f'db_files/{self.name}{field.name}.db', writeback=True)
-                    try:
-                        db_index[values[field.name]].append(values[self.key_field_name])
-                    except:
-                        db_index[values[field.name]] = list()
-                if values.get(field.name):
-                    data_table[str(values[self.key_field_name])][field.name] = values[field.name]
-                else:
-                    data_table[str(values[self.key_field_name])][field.name] = None
-        finally:
+        if data_table.get(str(values[self.key_field_name])):
             data_table.close()
+            raise ValueError
+        if len(self.fields) < len(values.keys()):
+            data_table.close()
+            raise ValueError
+        data_table[str(values[self.key_field_name])] = {}
+        for field in self.fields:
+            if self.is_index(field.name):
+                if values.get(field.name) == None:
+                    raise ValueError
+        for field in self.fields:
+            if self.is_index(field.name):
+                db_index = shelve.open(f'db_files/{self.name}{field.name}.db', writeback=True)
+                try:
+                    db_index[values[field.name]].append(values[self.key_field_name])
+                    db_index.close()
+                except:
+                    db_index[values[field.name]] = [values[self.key_field_name]]
+                    db_index.close()
+            if values.get(field.name):
+                data_table[str(values[self.key_field_name])][field.name] = values[field.name]
+            else:
+                data_table[str(values[self.key_field_name])][field.name] = None
+        data_table.close()
 
     def delete_record(self, key: Any) -> None:
         data_table = shelve.open(f"db_files/{self.name}.db", writeback=True)
@@ -81,9 +81,18 @@ class DBTable(db_api.DBTable):
                 data_table.close()
                 raise ValueError
             else:
+                for field in self.index_list:
+                    db_index = shelve.open(f'db_files/{self.name}{field}.db', writeback=True)
+                    if len(db_index[data_table[str(key)][field]]) == 1:
+                        del db_index[data_table[str(key)][field]]
+                    else:
+                        b = db_index[data_table[str(key)][field]].index(key)
+                        db_index[data_table[str(key)][field]].pop(b)
+                    db_index.close()
                 del data_table[str(key)]
         finally:
             data_table.close()
+
 
     def delete_records(self, criteria: List[SelectionCriteria]) -> None:
         data_table = shelve.open(f"db_files/{self.name}.db")
@@ -96,11 +105,25 @@ class DBTable(db_api.DBTable):
                 if data_table[list(data_table.keys())[0]].get(record.field_name):
                     if record.operator == '=':
                          record.operator = '=='
-                    if not eval(f"{data_table[record_in_table][record.field_name]}{record.operator}{str(record.value)}"):
-                        flag = 1
+                    try:
+                        if not eval(f"{data_table[record_in_table][record.field_name]}{record.operator}{str(record.value)}"):
+                            flag = 1
+                    except NameError:
+                        print("invalid")
             if not flag:
                 self.delete_record(record_in_table)
         data_table.close()
+
+
+
+    #
+    # def map_key(self, name, value):
+    #
+    # def is_criteria(self,critery,key):
+    #     if critery.operator == '=':
+    #         critery.operator = '=='
+    #     return not eval(f"{key}{critery.operator}{str(critery.value)}")
+
 
     def get_record(self, key: Any) -> Dict[str, Any]:
         data_table = shelve.open(f"db_files/{self.name}.db", writeback=True)
@@ -121,13 +144,24 @@ class DBTable(db_api.DBTable):
         for key_value in values.keys():
             if data_table[str(key)].get(key_value) == None:
                 raise ValueError
+        for field_index in self.index_list:
+            if values.get(field_index):
+                db_index = shelve.open(f'db_files/{self.name}{field_index}.db', writeback=True)
+                if len(db_index[data_table[str(key)][field_index]]) == 1:
+                    del db_index[data_table[str(key)][field_index]]
+                else:
+                    db_index[data_table[str(key)][field_index]].remove(key)
+                try:
+                    db_index[values[field_index]].append(key)
+                    db_index.close()
+                except:
+                    db_index[values[field_index]] = [key]
+                    db_index.close()
         data_table[str(key)].update(values)
         data_table.close()
 
     def query_table(self, criteria: List[SelectionCriteria]) -> List[Dict[str, Any]]:
-
         data_table = shelve.open(f"db_files/{self.name}.db")
-
         Selection_criteria_list = []
         for criter in criteria:
             if data_table[list(data_table.keys())[0]].get(criter.field_name)==None:
@@ -135,19 +169,36 @@ class DBTable(db_api.DBTable):
         for record_in_table in data_table.keys():
             flag = 0
             for record in criteria:
-                if data_table[list(data_table.keys())[0]].get(record.field_name):
-                    if record.operator == '=':
-                        record.operator = '=='
-                    try:
-                        if not eval(f'str(data_table[record_in_table][record.field_name]){record.operator}str(record.value)'):
-                            flag = 1
-                    except NameError:
-                        print("invalid Name")
-
+                if record.operator == '=':
+                    record.operator = '=='
+                    if not eval(f'str(data_table[record_in_table][record.field_name]){record.operator}str(record.value)'):
+                        flag = 1
             if not flag:
                 Selection_criteria_list.append(data_table[record_in_table])
-        data_table.close()
         return Selection_criteria_list
+
+
+
+
+    # def query_table(self, criteria: List[SelectionCriteria]) -> List[Dict[str, Any]]:
+    #     data_table = shelve.open(f"db_files/{self.name}.db")
+    #     Selection_criteria_list = []
+    #     for criter in criteria:
+    #         if data_table[list(data_table.keys())[0]].get(criter.field_name)==None:
+    #             raise ValueError
+    #     for record_in_table in data_table.keys():
+    #         flag = 0
+    #         for record in criteria:
+    #                 # if record.operator == '=':
+    #      record.operator = '=='
+    #     try:
+    #      if not eval(f'str(data_table[record_in_table][record.field_name]){record.operator}str(record.value)'):
+    #          flag = 1
+    #                 flag = is_criteria(record, data_table[record_in_table][record.field_name])
+    #                    print("invalid Name")
+    #         if not flag:
+    #             Selection_criteria_list.append(data_table[record_in_table])
+    #    return Selection_criteria_list
 
     def create_index(self, field_to_index: str) -> None:
         if field_to_index == self.key_field_name:
@@ -164,7 +215,7 @@ class DBTable(db_api.DBTable):
             try:
                 db_index[data_table[record][field_to_index]].append(record)
             except:
-                db_index[data_table[record][field_to_index]] = list()
+                db_index[data_table[record][field_to_index]] = [record]
         self.index_list.append(field_to_index)
         db = shelve.open("db_files/DB.db", writeback=True)
         db[self.name][2].append(field_to_index)
@@ -231,3 +282,33 @@ class DataBase(db_api.DataBase):
             fields_to_join_by: List[str]
     ) -> List[Dict[str, Any]]:
         raise NotImplementedError
+
+
+    # def delete_records(self, criteria: List[SelectionCriteria]) -> None:
+    #     data_table = shelve.open(f"db_files/{self.name}.db")
+    #     for criter in criteria:
+    #         if data_table[list(data_table.keys())[0]].get(criter.field_name)==None:
+    #              raise ValueError
+    #
+    #
+    #     for criter in criteria:
+    #         if self.is_index(criter.field_name):
+    #             db_index = shelve.open(f'db_files/{self.name}{criter.field_name}.db', writeback=True)
+    #             for key in db_index.keys():
+    #                 flag = is_criteria(criter,key)
+    #                 for
+    #             if not flag:
+    #                 self.delete_record(record_in_table)
+    #
+    #     for record_in_table in data_table.keys():
+    #         flag = 0
+    #         for record in criteria:
+    #             if data_table[list(data_table.keys())[0]].get(record.field_name):
+    #                 # if record.operator == '=':
+    #                 #      record.operator = '=='
+    #                 # if not eval(f"{data_table[record_in_table][record.field_name]}{record.operator}{str(record.value)}"):
+    #                     flag = 1
+    #                     flag = is_criteria(record,data_table[record_in_table][record.field_name])
+    #         if not flag:
+    #             self.delete_record(record_in_table)
+    #     data_table.close()
